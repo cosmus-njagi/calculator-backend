6c6c299ee07db5f1b1c192a5bd5957809ae22b57
Cosmus <njagicosmus@gmail.com>
2023-06-24 22:20:39 +0300

    added imports/configurations for filter token

--- src/main/java/com/pycs/calculatorbackend/configuration/JwtRequestFilter.java
+++ src/main/java/com/pycs/calculatorbackend/configuration/JwtRequestFilter.java
@@ -1,11 +1,27 @@
 package com.pycs.calculatorbackend.configuration;
 
+import com.pycs.calculatorbackend.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author njagi
  * @Date 24/06/2023
  */
 @Component
-public class JwtRequestFilter extends OncePerRequestFilter {
+public class JwtRequestFilter extends OncePerRequestFilter implements Filter {
 
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
