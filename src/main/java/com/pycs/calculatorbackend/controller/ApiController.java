a6c4a3d0d0e2e52dd47f021972a187f1d2b04108
Cosmus <njagicosmus@gmail.com>
2023-06-25 13:19:52 +0300

    added authenticate endpoint

--- src/main/java/com/pycs/calculatorbackend/controller/ApiController.java
+++ src/main/java/com/pycs/calculatorbackend/controller/ApiController.java
@@ -4,12 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pycs.calculatorbackend.configuration.JwtTokenUtil;
 import com.pycs.calculatorbackend.model.JwtRequest;
 import com.pycs.calculatorbackend.model.JwtResponse;
+import com.pycs.calculatorbackend.model.Response;
 import com.pycs.calculatorbackend.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -17,8 +22,8 @@ import org.springframework.security.core.userdetails.UserDetails;
  * @author njagi
  * @Date 24/06/2023
  */
+@CrossOrigin("*")
 @RestController
-@CrossOrigin
 @RequestMapping("/api")
 public class ApiController {
 
@@ -31,20 +36,42 @@ public class ApiController {
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
 
-    @PostMapping("/authenticate")
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authRequest) throws Exception {
-        System.out.println("Request params ###"+ new ObjectMapper().writeValueAsString(authRequest));
-        try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
-        } catch (BadCredentialsException e) {
-            throw new Exception("Invalid username or password");
-        }
+    private Response apiResponse = new Response();
 
-        final UserDetails userDetails = (UserDetails) userDetailsService
-                .loadUserByUsername(authRequest.getUsername());
-        final String token = jwtTokenUtil.generateToken(userDetails);
-        return ResponseEntity.ok(new JwtResponse(token));
+    private void authenticate(String username, String password) throws AuthenticationException {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new DisabledException("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("INVALID_CREDENTIALS", e);
+        }
     }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+        try {
+            String authUsername = authenticationRequest.getUsername();
+            String authPassword = authenticationRequest.getPassword();
+            System.out.println("username received ## " + authenticationRequest.getUsername());
+            authenticate(authUsername, authPassword);
+            final UserDetails userDetails = userDetailsService.loadUserByUsername(authUsername);
+            final String token = jwtTokenUtil.generateToken(userDetails);
+            return ResponseEntity.ok(new JwtResponse(token));
+        } catch (DisabledException e) {
+            apiResponse.setReturnCode("01");
+            apiResponse.setMessage("oops! internal error occurred");
+            return new ResponseEntity<>(apiResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (BadCredentialsException e) {
+            apiResponse.setReturnCode("01");
+            apiResponse.setMessage("oops! invalid credentials");
+            return new ResponseEntity<>(apiResponse, HttpStatus.UNAUTHORIZED);
+        } catch (AuthenticationException e) {
+            apiResponse.setReturnCode("01");
+            apiResponse.setMessage("oops! internal error occurred");
+            return new ResponseEntity<>(apiResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
 
