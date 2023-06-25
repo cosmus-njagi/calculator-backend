523c6a34e995577c25fc1fdc9b1caf5455ac4664
Cosmus <njagicosmus@gmail.com>
2023-06-25 13:19:19 +0300

    exclude gateway authenticate from checking for token

--- src/main/java/com/pycs/calculatorbackend/configuration/JwtRequestFilter.java
+++ src/main/java/com/pycs/calculatorbackend/configuration/JwtRequestFilter.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -36,19 +35,33 @@ public class JwtRequestFilter extends OncePerRequestFilter implements Filter {
         String username = null;
         String token = null;
 
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            token = authorizationHeader.substring(7);
-            username = jwtTokenUtil.getUsernameFromToken(token);
-        }
+        // Check if the request is for /api/authenticate
+        if (!request.getRequestURI().equals("/api/authenticate")) {
 
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            // Every valid token should start with the keyword "Bearer"
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                token = authorizationHeader.substring(7);
+                try {
+                    username = jwtTokenUtil.getUsernameFromToken(token);
+                } catch (Exception e) {
+                    System.out.println("JWT Token has expired" + e);
+                    SecurityConfig.sendError(response, e, HttpServletResponse.SC_FORBIDDEN, "Not authorized resources");
+                    return; // Return immediately to bypass further processing for this request
+                }
+            } else {
+                System.out.println("JWT Token does not begin with Bearer String");
+            }
 
-            if (jwtTokenUtil.validateToken(token, userDetails)) {
-                UsernamePasswordAuthenticationToken authenticationToken =
-                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            // Check if the username is missing
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+                if (jwtTokenUtil.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+                    usernamePasswordAuthenticationToken
+                            .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
             }
         }
 
